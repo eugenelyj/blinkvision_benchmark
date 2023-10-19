42df3952c7cc8b5933f488aa48f52a4060e62fbd
eugenelyj <496734971@qq.com>
2023-10-19 11:55:00 +0800

    add download script for flow_v1

---
+++ flow_v1/download_test.sh
@@ -0,0 +1,2 @@
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/test.zip
+unzip test.zip
---
+++ flow_v1/download_train.sh
@@ -0,0 +1,16 @@
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-aa
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ab
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ac
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ad
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ae
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-af
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ag
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ah
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ai
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-aj
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ak
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-al
+wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-am
+
+cat train.zip.part-* > train.zip
+unzip trainzip
