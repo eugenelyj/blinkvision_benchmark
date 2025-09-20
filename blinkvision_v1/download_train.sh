24196f9190ee62819c4adcdc3faa30dbcc8f32f8
eugenelyj <496734971@qq.com>
2025-09-20 21:48:56 +0800

    update download_train.sh

--- blinkvision_v1/download_train.sh
+++ blinkvision_v1/download_train.sh
@@ -8,4 +8,8 @@ huggingface-cli login
 # Download the dataset
 huggingface-cli download $REPO_ID --repo-type dataset --local-dir=./data
 
+# Once you have downloaded, you need to concatenate the downloaded file chunks and decompress them. For example:
+# cat outdoor_train_event_part_a* > outdoor_train_event.tar.gz
+# tar -xvzf outdoor_train_event.tar.gz
+
 echo "Dataset downloaded successfully to ./data directory"
