cca5394a19ef1d80cf9d8e2fdcf3f5c12d5bea6b
eugenelyj <496734971@qq.com>
2026-09-24 16:41:50 +0800

    Update download_train.sh

--- blinkflow_v1/download_train.sh
+++ blinkflow_v1/download_train.sh
@@ -1,16 +1,6 @@
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-aa
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ab
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ac
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ad
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ae
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-af
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ag
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ah
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ai
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-aj
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-ak
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-al
-wget -r ftp://ivp-db.ee.cuhk.edu.hk/hsli/BlinkFlow/train.zip.part-am
-
+modelscope download \
+  --dataset eugenelyj96/blinkflow \
+  --include "train/*" \
+  --local_dir ./blinkflow
 cat train.zip.part-* > train.zip
-unzip trainzip
+unzip train.zip
